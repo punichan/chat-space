7076ad7380f4aacd74246681445c276a883df30c
市川真巳 <masamikai4580@gmail.com>
2019-10-23 19:05:10 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -26,6 +26,7 @@ $(document).on('turbolinks:load', function() {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
+    console.log(this);
     $.ajax({
       url: url,
       type: "POST",
@@ -45,49 +46,54 @@ $(document).on('turbolinks:load', function() {
       $('.contents__right__footer__textbox__btn').attr('disabled',false);
     })
     .fail(function(){
-      alert('error');
+      alert('erro');
       $('.contents__right__footer__textbox__btn').attr('disabled',false);
     })
   });
 
-  //  自動更新
-  var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    last_message_id = $(".contents__right__body__box:last").data("id");
-    console.log(last_message_id
-      )
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){ 
-    $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
+  console.log("Q");
 
-    .done(function(messages) {
-      //追加するHTMLの入れ物を作る
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-      //メッセージが入ったHTMLを取得
-      //メッセージを追加
-      messages.forEach(function(message){
-        var insertHTML = buildMessageHTML(message);
-        console.log("1");
-        $('.contents__right__body').append(insertHTML);
-        $('.contents__right__body').animate({
-          scrollTop: $('.contents__right__body')[0].scrollHeight
-          
+  //  自動更新
+  function reloadMessages() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    last_message_id = $(".contents__right__body__box:last").data("id");
+      console.log(last_message_id);
+    
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+
+      .done(function(messages) {
+        //追加するHTMLの入れ物を作る
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        //メッセージが入ったHTMLを取得
+        //メッセージを追加
+        messages.forEach(function(message){
+          var insertHTML = buildMessageHTML(message);
+          $('.contents__right__body').append(insertHTML);
+          $('.contents__right__body').animate({
+            scrollTop: $('.contents__right__body')[0].scrollHeight
+            
+          });
         });
       })
-    })
 
-    .fail(function() {
-     alert('error');
-    });
+      .fail(function() {
+        alert('error');
+
+      });
+    }
   };
-};
-  setInterval(reloadMessages,10000);
+  console.log("AA");
+  setInterval(reloadMessages,3000);
 });
+
+
 
